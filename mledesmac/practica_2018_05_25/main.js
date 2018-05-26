936699553cea188299ea53d5b289bdda2e81fc1b
Mauricio eduardo Ledesma cortes <mledesma@MacBook-Pro-de-Mauricio.local>
2018-05-25 20:41:28 -0500

    Uploading the task of today wit the select, modal and tooltip

--- mledesmac/practica_2018_05_25/main.js
+++ mledesmac/practica_2018_05_25/main.js
@@ -1,12 +1,63 @@
 "use strict";
 
+// Variable section
+let selectHTML = document.getElementById("selectSection");
+let buttonHTML = document.getElementById("modalSection");
+let closeHTML = document.getElementById("closeModal");
+let closeButtonHTML  = document.getElementById("closeButtonModal");
+let helpHTML = document.getElementById("helpSection");
+let flagModal = false;
+
+/* Custom text for the modal*/
+document.getElementById('titleModal').innerHTML = 'Modal con título';
+document.getElementById('bodyModal').innerHTML = 'Cuerpo de modal';
+document.getElementById('footerModal').innerHTML = 'Footer del modal';
+
+// Functions section
 const showDivSelector = () =>{
     let optionHTML = document.getElementById("optionSection");
     if(optionHTML.style.display === 'none')
         optionHTML.style.display = 'block';
     else
         optionHTML.style.display = 'none';
-}
+};
 
-let selectHTML = document.getElementById("selectSection");
-selectHTML.addEventListener('click', showDivSelector)
+const showDivModal = () => {
+    let modalHTML = document.getElementById("modalWindow");
+    flagModal = true;
+    modalHTML.style.display = 'block';
+
+};
+
+const closeModal = () => {
+    let modalHTML = document.getElementById("modalWindow");
+
+    modalHTML.style.display = 'none';
+};
+
+
+const closeModalBody = () => {
+    let modalHTML = document.getElementById("modalWindow");
+    alert('');
+    if(flagModal === true)
+        modalHTML.style.display = 'none';
+};
+
+const showToolTip = () => {
+    let toolTipHTML = document.getElementById('toolTip');
+
+    if(toolTipHTML.style.display === 'none')
+        toolTipHTML.style.display = 'block';
+    else
+        toolTipHTML.style.display = 'none';
+
+};
+
+// Listener Event
+selectHTML.addEventListener('click', showDivSelector);
+buttonHTML.addEventListener('click', showDivModal);
+closeHTML.addEventListener('click', closeModal);
+closeButtonHTML.addEventListener('click', closeModal);
+helpHTML.addEventListener('mouseover', showToolTip);
+helpHTML.addEventListener('mouseout', showToolTip);
+//document.body.addEventListener('click',closeModalBody, false);
