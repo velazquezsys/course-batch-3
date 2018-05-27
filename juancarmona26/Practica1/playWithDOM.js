5f7dbffdac328fa1706d62386702587ab7c65c22
juancarmona26 <juan.carmona.mtz@gmail.com>
2018-05-27 10:31:24 -0500

    creación de modal

--- juancarmona26/Practica1/playWithDOM.js
+++ juancarmona26/Practica1/playWithDOM.js
@@ -1,15 +1,32 @@
 
 
+'use strict';
+
 const arrayOfDinosaurus = ['allosaurus', 'brontosaurus', 'carcharodontosaurus','diplodocus'];
 const value = 'value';
+let divOptionMenu;
+let divOptionValues;
+let modal;
+let btn;
+let span;
+let btnModalClose;
+getHTMLElements();
+
+function getHTMLElements(){
+    divOptionMenu = document.getElementById('optionMenu');
+    divOptionValues = document.getElementById('optionValue');
+    modal = document.getElementById('myModal');
+    btn = document.getElementById("myBtn");
+    span = document.getElementsByClassName("close")[0];   
+
+}
 
 
 
 function createOptionsInOptionMenuDiv() {
 
     for(const iterable of arrayOfDinosaurus) {
-        const divOptionMenu = document.getElementById('optionMenu');
-        const divOptionValues = document.getElementById('optionValue');
+      
         
         const option = document.createElement('OPTION');
         option.setAttribute(value, iterable);
@@ -25,13 +42,30 @@ function createOptionsInOptionMenuDiv() {
 };
 
 function setValue(value) {
-    console.log("si entrea");
-    const divOptionMenu = document.getElementById('optionMenu');
-    const divOptionValues = document.getElementById('optionValue');
+    console.log("si entra")
     const textElement = document.getElementById('demo');
     textElement.innerHTML = value;   
     divOptionMenu.style.display = 'block';
 
     divOptionValues.innerHTML = '';  
     
+};
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
