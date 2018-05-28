838a51d42e8c79a8691929fd2d3800a0148d44f3
Emanuel <dive.0420@gmail.com>
2018-05-27 22:10:02 -0500

    ejercicios finalizados

--- dive0420/ec25052018/main.js
+++ dive0420/ec25052018/main.js
@@ -1,40 +1,77 @@
-const dropDown = document.getElementById("dropDown");
-const launch = document.getElementById("launch");
-const opciones = document.getElementsByClassName("opcion");
-const modal = document.getElementById("modal");
-const texto = document.getElementById("texto");
-const close = document.getElementById("close");
-const principal = document.getElementById("principal")
-let texto = ``
+const dropDown = document.getElementById('dropDown');
+const launch = document.getElementById('launch');
+const opciones = document.getElementsByClassName('opcion');
+const modal = document.getElementById('modal');
+const bodyDiv = document.getElementById('bodyDiv');
+const close = document.getElementById('close');
+const principal = document.getElementById('principal');
+const todo = document.getElementById('todo');
 
+bodyDiv.innerText = `Un texto es una composición de signos codificados en un sistema de escritura que forma una unidad de sentido.
+                    También es una composición de caracteres imprimibles
+                     (con grafema) generados por un algoritmo de cifrado que, 
+                     aunque no tienen sentido para cualquier persona, sí puede 
+                     ser descifrado por su destinatario original. En otras palabras,
+                      un texto es un entramado de signos con una intención comunicativa
+                       que adquiere sentido en determinado contexto.`;
+const tooltip = document.getElementById('tooltip');
 
-dropDown.addEventListener('click', function () {
-    let opciones = this.nextElementSibling;
-    if (opciones.style.display === "block") {
-        opciones.style.display = "none";
+dropDown.addEventListener('click',  (e) => {
+    let opcionDiv = dropDown.nextElementSibling;
+    if (opcionDiv.style.display === 'block') {
+        opcionDiv.style.display = 'none';
     } else {
-        opciones.style.display = "block";
-        this.style.display = "none";
+        opcionDiv.style.display = 'block';
+        dropDown.style.display = 'none';
     }
 });
 
 const opcionesClick = () => {
     let i;
     for (i = 0; i < opciones.length; i++) {
-        opciones[i].addEventListener("click", function() {
+        opciones[i].addEventListener('click', function () {
             dropDown.innerText = this.innerText;
-            this.parentElement.style.display = "none";
-            dropDown.style.display = "block";
+            this.parentElement.style.display = 'none';
+            dropDown.style.display = 'block';
         });
     }
 }
 
-launch.addEventListener('click', function () {
-    modal.style.display = "block";
+launch.addEventListener('click', (e) => {
+    principal.style.display = 'none';
+    todo.style.display = 'block'
+    modal.style.display = 'block';
 });
 
-close.addEventListener('click', function () {
-    modal.style.display = "none";
+close.addEventListener('click', (e) => {
+    modal.style.display = 'none';
+    principal.style.display = 'block';
+});
+
+todo.addEventListener('click', (e) => {
+    if(todo == e.target){
+        principal.style.display = 'block';
+        todo.style.display = 'none'
+        modal.style.display = 'none';
+    }
+})
+
+tooltip.addEventListener('mouseenter', (e) => {
+    let tooltipText = document.getElementById('tooltipText');
+    tooltipText.style.visibility = 'visible';
+    tooltipText.style.width = '120px';
+    tooltipText.style.backgroundColor = 'black';
+    tooltipText.style.color = '#fff';
+    tooltipText.style.padding = '5px 0px 0px 0px';
+    tooltipText.style.borderRadius = '6px';
+
+    tooltipText.style.position = 'absolute';
+    tooltipText.style.zIndex = '1';
+});
+
+tooltip.addEventListener('mouseleave', (e) => {
+    let tooltipText = document.getElementById('tooltipText');
+    tooltipText.style.visibility = 'hidden';
 });
 
 
