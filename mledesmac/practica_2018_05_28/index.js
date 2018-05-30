420def98cce2283dd6ed45f8feed45116f0bde7c
Mauricio eduardo Ledesma cortes <mledesma@MacBook-Pro-de-Mauricio.local>
2018-05-29 19:03:59 -0500

    Adding the changes

--- mledesmac/practica_2018_05_28/index.js
+++ mledesmac/practica_2018_05_28/index.js
@@ -5,14 +5,14 @@ const tableUsers = document.getElementById('tableUsers');
 const closeHTML = document.getElementById('closeModal');
 const closeButtonHTML = document.getElementById('closeButtonModal');
 
-const  doFetchMau = (urlAddress)  => {
+const doFetchMau = (urlAddress) => {
     return fetch(urlAddress)
-        .then(function(res) {
+        .then(function (res) {
             return res.json();
         })
-        .then(function(response) {
+        .then(function (response) {
             let htmlResponse = (response.results);
-            for(const person of htmlResponse){
+            for (const person of htmlResponse) {
                 const column = `<tr><td>${person.email}</td><td>${person.name.first}</td><td>${person.name.last}</td></tr>`;
                 tableUsers.innerHTML += column;
             }
@@ -23,19 +23,19 @@ const  doFetchMau = (urlAddress)  => {
 };
 
 
-    const showDivModal = () => {
+const showDivModal = () => {
 
-        let modalHTML = document.getElementById('modalWindow');
-        flagModal = true;
-        modalHTML.style.display = 'block';
+    let modalHTML = document.getElementById('modalWindow');
+    flagModal = true;
+    modalHTML.style.display = 'block';
 
-    };
+};
 
-    const closeModal = () => {
-        const modalHTML = document.getElementById("modalWindow");
-        modalHTML.style.display = 'none';
-        flagModal = false;
-    };
+const closeModal = () => {
+    const modalHTML = document.getElementById("modalWindow");
+    modalHTML.style.display = 'none';
+    flagModal = false;
+};
 
 
 doFetchMau('https://randomuser.me/api/?results=20');
