3015aafab0b859869e5b7f2556568875f4d1fb18
Guillermo Quezada Martínez <guillermoqm_1994@hotmail.com>
2018-06-05 21:04:29 -0500

    adding unit test

---
+++ gQuezada/unitaryTest/src/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const sort = (array) => {
+  for(let i = 0; i < array.length; i++) {
+    let min = i;
+    for(let j = i + 1; j < array.length; j++) {
+      if(array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if(i !== min) {
+      [array[i], array[min]] = [array[min], array[i]];
+    }
+  }
+  return array;
+}
+
+module.exports = sort;
