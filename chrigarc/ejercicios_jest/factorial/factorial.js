4bcb5ea37d9f225cd88e1858d2995f8bcf4fa1a9
Christian Garcia <chrigarc@chrigarc-laptop-dell>
2018-06-05 19:42:33 -0500

    Ejercicio de implementación de pruebas unitarias de algoritmo matematico

---
+++ chrigarc/ejercicios_jest/factorial/factorial.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const factorial = (parameter, acumulador = 1) => {
+    if(!Number.isInteger(parameter)){
+        return {error: '001', message : 'Parameter not is number integer'};
+    }
+    if(parameter < 0){
+        return {error: '002', message : 'Parameter not >= 0'};
+    }
+    switch(parameter){
+        case 0:
+        case 1:
+            return acumulador;
+            break;
+        default:
+            return factorial(parameter-1, parameter * acumulador);
+    }
+};
+
+module.exports = {factorial};
