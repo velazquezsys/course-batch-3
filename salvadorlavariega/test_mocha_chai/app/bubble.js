08f6d0a27e99657150e53073988f472c56cdd879
Slavariega <salvador.lav@gmail.com>
2018-06-05 20:51:26 -0500

    Bubble Sort terminado

--- salvadorlavariega/test_mocha_chai/app/bubble.js
+++ salvadorlavariega/test_mocha_chai/app/bubble.js
@@ -1,22 +1,40 @@
- exports.addTested = (value) => {
-    var result = value + " tested";
-    return result;
-};
+'use strict';
 
- exports.isArray = (array) => {
-     return array instanceof Array;
- };
-
- exports.isNumericArray = (array) => {
-    if(exports.isArray(array)){
-        if(array.length > 0){
-            for(const element of array){
-                if(isNaN(element)){
-                    return false;
+const bubbleSort = (array) => {
+    if(isNumericArray(array)){
+        for(let position = 1; position < array.length; position++ ){
+            for(let index = 0; index < array.length - position; index ++){
+                if( array[index] > array[index + 1] ){
+                    const minor = array[index];
+                    array[index] = array[index + 1];
+                    array[index + 1] = minor;
                 }
             }
-            return true;
         }
+        return array;
+    }
+    return [];
+};
+
+
+const isNumericArray = (array) => {
+    if(isArray(array) && array.length > 0){
+        for(const element of array){
+            if(isNaN(element)){
+                return false;
+            }
+        }
+        return true;
     }
     return false;
- };
+};
+
+const isArray = (array) => {
+    return array instanceof Array;
+};
+
+module.exports =  {
+    isArray: isArray,
+    isNumericArray : isNumericArray,
+    bubbleSort: bubbleSort
+};
--- salvadorlavariega/test_mocha_chai/test/bubble_assert.js
+++ salvadorlavariega/test_mocha_chai/test/bubble_assert.js
@@ -1,81 +1,100 @@
-let assert    = require("chai").assert;
-let bubble = require("../app/bubble");
+'use strict';
 
-describe("Bubble should accept only array objects ", function() {
-    describe("Check isArray Function: ", function() {
-        it("return true if the parameter in an array object", function() {
+const assert    = require("chai").assert;
+const expect    = require("chai").expect;
+const bubble = require("../app/bubble");
+
+describe("Bubble should accept only array objects ", () => {
+    describe("Check isArray Function: ", () => {
+        it("return true if the parameter in an array object", () => {
             const myArray = [12,4,62,56,8,23];
-            result   = bubble.isArray(myArray);
+            const result   = bubble.isArray(myArray);
             assert.equal(result, true);
         });
-        it("return false if a variable integer is not an Array Object", function() {
+        it("return false if a variable integer is not an Array Object",() => {
             const myArray = 50;
-            result   = bubble.isArray(myArray);
+            const result   = bubble.isArray(myArray);
             assert.equal(result, false);
         });
-        it("return false if a String as parameter is not an Array Object", function() {
+        it("return false if a String as parameter is not an Array Object", () => {
             const myArray = 'string'
-            result   = bubble.isArray(myArray);
+            const result   = bubble.isArray(myArray);
             assert.equal(result, false);
         });
-        it("return false if the undefined variable other is not an Array Object", function() {
+        it("return false if the undefined variable other is not an Array Object", () =>{
             let other;
-            result   = bubble.isArray(other);
+            const result   = bubble.isArray(other);
             assert.equal(result, false);
         });
-        it("return false if null as parameter is not an Array Object", function() {
-            result   = bubble.isArray(null);
+        it("return false if null as parameter is not an Array Object", () => {
+            const result   = bubble.isArray(null);
             assert.equal(result, false);
         });
-       /* it("Check the returned value using: assert.typeOf(value,'value'): ", function() {
-            result   = bubble.addTested("text");
-            assert.typeOf(result, "string");
-        });
-        it("Check the returned value using: assert.lengthOf(value,'value'): ", function() {
-            result   = bubble.addTested("text");
-            assert.lengthOf(result, 11);
-        });*/
     });
 
 });
 
-describe("Bubble should accept only Integer array objects ", function() {
+describe("Bubble should accept only Integer array objects ",  () => {
     describe("Check isNumericArray Function: ", function() {
-        it("return true if the parameter in an numeric array object", function() {
+        it("return true if the parameter is a numeric array object", () =>{
             const myArray = [12,4,62,56,8,23];
-            result   = bubble.isNumericArray(myArray);
+            const result   = bubble.isNumericArray(myArray);
             assert.equal(result, true);
         });
-        it("return false if new Array as parameter is not a numeric array object", function() {
+        it("return false if new Array as parameter is not a numeric array object",() => {
             const myArray = new Array;
-            result   = bubble.isNumericArray(myArray);
+            const result   = bubble.isNumericArray(myArray);
             assert.equal(result, false);
         });
-        it("return false if ['s','a','d'] as parameter is not a numeric array object", function() {
+        it("return false if ['s','a','d'] as parameter is not a numeric array object", () => {
             const myArray = ['s','a','d'];
-            result   = bubble.isNumericArray(myArray);
+            const result   = bubble.isNumericArray(myArray);
             assert.equal(result, false);
         });
-        it("return false if [12,4,62,56,8,'a'] as parameter is not a numeric array object", function() {
+        it("return false if [12,4,62,56,8,'a'] as parameter is not a numeric array object",() => {
             const myArray = [12,4,62,56,8,'a'];
-            result   = bubble.isNumericArray(other);
+            const result   = bubble.isNumericArray(myArray);
             assert.equal(result, false);
         });
-        it("return false if [] as parameter is not a numeric array object", function() {
+        it("return false if [] as parameter is not a numeric array object", () => {
             const myArray = [];
+            const result   = bubble.isNumericArray(myArray);
             assert.equal(result, false);
         });
-        /* it("Check the returned value using: assert.typeOf(value,'value'): ", function() {
-             result   = bubble.addTested("text");
-             assert.typeOf(result, "string");
-         });
-         it("Check the returned value using: assert.lengthOf(value,'value'): ", function() {
-             result   = bubble.addTested("text");
-             assert.lengthOf(result, 11);
-         });*/
     });
 
+});
 
 
+describe("Bubble Sort must order an integer array from least to greatest",  () =>{
+    describe("Check bubbleSort Function: ",  () => {
+        it("return empty array [] if the array is not numeric or is empty",  () => {
+            const myArray = [];
+            const result   = bubble.bubbleSort(myArray);
+            expect([]).to.deep.equal(result);
+        });
+
+        it("return the array [1,5,3,2] ordened like [1,2,3,5]",  () => {
+            const myArray = [1,5,3,2];
+            const ordenedArray = [1,2,3,5];
+            const result   = bubble.bubbleSort(myArray);
+            expect(ordenedArray).to.deep.equal(result);
+        });
+
+        it("return the array  [29,45,21,32,12,9,45,23,1,45] ordened like [1,9,12,21,23,29,32,45,45,45]",  () => {
+            const myArray = [29,45,21,32,12,9,45,23,1,45];
+            const result   = bubble.bubbleSort(myArray);
+            expect([1,9,12,21,23,29,32,45,45,45]).to.deep.equal(result);
+        });
+
+        it("return the array [345,123,5645,7678,123,4323,23,56,7867] ordened like [23,56,123,123,345,4323,5645,7678,7867]",
+            () => {
+            const myArray = [345,123,5645,7678,123,4323,23,56,7867];
+            const result   = bubble.bubbleSort(myArray);
+            expect([23, 56, 123, 123, 345, 4323, 5645, 7678, 7867]).to.deep.equal(result);
+            }
+         );
+
+    });
 
 });
