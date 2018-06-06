6b95b0b5ea4bc2cef973d8a18eafae12bdb7354f
Alfonso Rios <manuelalfonso.rios.contractor@bbva.com>
2018-06-05 20:56:16 -0500

    refactor(*): Iteration for append nodes

--- gQuezada/practica1/src/Employee.js
+++ gQuezada/practica1/src/Employee.js
@@ -1,42 +1,32 @@
 class Employee {
-    constructor(name, email, phone, cell, sha256) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.cell = cell;
-        this.sha256 = sha256;
-    }
+   constructor(name, email, phone, cell, sha256) {
+      this.name = name;
+      this.email = email;
+      this.phone = phone;
+      this.cell = cell;
+      this.sha256 = sha256;
+   }
 
-    getHTML() {
-        let tr = document.createElement('tr');
-        let td=[];
-        let tn=[];
+   getHTML() {
+      const tr = document.createElement('tr');
+      let td = [];
+      let textNode = [];
 
-        tn.push(document.createTextNode(this.name));
-        td.push(document.createElement('td'));
-        td[0].appendChild(tn[0]);
-        tr.appendChild(td[0]);
+      const headersNames = Object.getOwnPropertyNames(this);
 
-        tn.push(document.createTextNode(this.email));
-        td.push(document.createElement('td'));
-        td[1].appendChild(tn[1]);
-        tr.appendChild(td[1]);
+      const headersNodes = document.querySelector('#employees thead tr').childNodes;
+      let columnsIndex = 0;
 
-        tn.push(document.createTextNode(this.phone));
-        td.push(document.createElement('td'));
-        td[2].appendChild(tn[2]);
-        tr.appendChild(td[2]);
+      for (const header of headersNodes) {
+         if (header.nodeName === 'TH') {
+            textNode.push(document.createTextNode(this[headersNames[ columnsIndex ]]));
+            td.push(document.createElement('td'));
+            td[ columnsIndex ].appendChild(textNode[ columnsIndex ]);
+            tr.appendChild(td[ columnsIndex ]);
+            columnsIndex++;
+         }
+      }
 
-        tn.push(document.createTextNode(this.cell));
-        td.push(document.createElement('td'));
-        td[3].appendChild(tn[3]);
-        tr.appendChild(td[3]);
-
-        tn.push(document.createTextNode(this.sha256));
-        td.push(document.createElement('td'));
-        td[4].appendChild(tn[4]);
-        tr.appendChild(td[4]);
-
-        return tr;
-    }
+      return tr;
+   }
 }
--- gQuezada/practica1/src/index.js
+++ gQuezada/practica1/src/index.js
@@ -1,27 +1,29 @@
 (() => {
-    (async () => {
-        var rawResponse = await fetch('https://randomuser.me/api/', {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        });
-        var content = await rawResponse.json();
+   (async () => {
+      var rawResponse = await fetch('https://randomuser.me/api/', {
+         method: 'GET',
+         headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+         }
+      });
+      var content = await rawResponse.json();
 
-        var employees = [];
+      var employees = [];
 
-        content.results.forEach(function (element) {
-            var e = new Employee(element.name.first, element.email, element.phone, element.cell, element.login.sha256);
-            employees.push(e);
-        });
+      content.results.forEach(function(element) {
+         var e = new Employee(element.name.first,
+            element.email, element.phone, element.cell,
+            element.login.sha256);
+         employees.push(e);
+      });
 
 
-        for (let employee of employees){
-            document.querySelector('#employees tbody').appendChild(employee.getHTML());
-        }
-        
-        console.log(employees);
+      for (let employee of employees) {
+         document.querySelector('#employees tbody').appendChild(employee.getHTML());
+      }
 
-    })();
+      console.log(employees);
+
+   })();
 })();
