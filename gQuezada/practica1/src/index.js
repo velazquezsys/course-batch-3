9116796bb8fd5835a1f0f4b5aa99ca92bda9a3a3
Guillermo Quezada Martínez <guillermoqm_1994@hotmail.com>
2018-06-05 21:12:52 -0500

    from ES5 to ES6

--- gQuezada/practica1/src/index.js
+++ gQuezada/practica1/src/index.js
@@ -1,18 +1,18 @@
 (() => {
    (async () => {
-      var rawResponse = await fetch('https://randomuser.me/api/', {
+      let rawResponse = await fetch('https://randomuser.me/api/', {
          method: 'GET',
          headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
          }
       });
-      var content = await rawResponse.json();
+      let content = await rawResponse.json();
 
-      var employees = [];
+      let employees = [];
 
-      content.results.forEach(function(element) {
-         var e = new Employee(element.name.first,
+      content.results.forEach((element) ()=> {
+         let e = new Employee(element.name.first,
             element.email, element.phone, element.cell,
             element.login.sha256);
          employees.push(e);
@@ -22,8 +22,6 @@
       for (let employee of employees) {
          document.querySelector('#employees tbody').appendChild(employee.getHTML());
       }
-
-      console.log(employees);
 
    })();
 })();
