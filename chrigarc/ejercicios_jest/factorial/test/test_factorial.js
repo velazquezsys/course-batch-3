231e657e6cca4fba3715b9b36f57b1fbdeea1409
Christian Garcia <chrigarc@chrigarc-laptop-dell>
2018-06-05 21:01:00 -0500

    Ejercicio de implementación de pruebas unitarias de algoritmo matematico

--- chrigarc/ejercicios_jest/factorial/test/test_factorial.js
+++ chrigarc/ejercicios_jest/factorial/test/test_factorial.js
@@ -1,20 +1,20 @@
 'use strict';
 const assert    = require('chai').assert;
 const factorial = require('../factorial').factorial;
-describe("Pruebas unitarias para la funcion factorial: ", () => {
-    it("test_parametro_invalido ", () => {
+describe('Pruebas unitarias para la funcion factorial: ', () => {
+    it('test_parametro_invalido ', () => {
         const parameter = '';
         const result_factorial = factorial(parameter);
         const expect_value = {error: '001', message : 'Parameter is not a integer number'};
         assert.deepEqual(result_factorial, expect_value);
     });
-    it("test_parametro_correcto ", () => {
+    it('test_parametro_correcto ', () => {
         const parameter = 20;
         const result_factorial = factorial(parameter);
         const expect_value = 2432902008176640000;
         assert.equal(result_factorial, expect_value);
     });
-    it("test_parametro_entero_negativo ", () => {
+    it('test_parametro_entero_negativo ', () => {
         const parameter = -2;
         const result_factorial = factorial(parameter);
         const expect_value = {error: '002', message : 'Parameter not >= 0'};
