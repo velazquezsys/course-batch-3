908db8280d8162689b85ea441f922e05d1ca191d
Guillermo Quezada Martínez <guillermoqm_1994@hotmail.com>
2018-06-13 22:00:52 -0500

    Add command

--- gQuezada/practica3/src/main.js
+++ gQuezada/practica3/src/main.js
@@ -18,20 +18,86 @@ const removeBlanks = (string) => {
   const withoutLines = string.replace(/\n/g,'')
   const withoutTabs = withoutLines.replace(/\t/g, '');
   const withoutSpaces = withoutLines.replace(/\s{2,}/g, '');
-  return withoutSpaces;
+  return withoutSpaces.trim();
 };
 
-const getHTMLTree = () => {
+const getHTMLTree = (lexicalComponents) => {
+  const stack = [];
+  for(let lexicalComponent of lexicalComponents) {
+    switch (lexicalComponent.type) {
+      case 'openTag':
 
+      break;
+      case 'doctype':
+
+      break;
+      case 'textNode':
+
+      break;
+      case 'closeTag':
+
+      break;
+    }
+  }
+};
+
+const getLexicalComponents = (content) => {
+
+  const lexicalComponents = [];
+
+  const lexicalComponentsExpressions = [
+                                          {
+                                            regularExpresion: /^<[^<>/!]+>/,
+                                            type: 'openTag'
+                                          },
+                                          {
+                                            regularExpresion: /^<![^<>/!]+>/,
+                                            type: 'doctype'
+                                          },
+                                          {
+                                            regularExpresion: /^[^<]+/,
+                                            type: 'textNode'
+                                          },
+                                          {
+                                            regularExpresion: /^<\/[^<>/!]+>/,
+                                            type: 'closeTag'
+                                          }
+                                      ];
+
+
+  let finished = null;
+
+  do{
+    finished = true;
+    for(let lexicalComponent of lexicalComponentsExpressions) {
+      const result = lexicalComponent.regularExpresion.exec(content);
+      if(result) {
+        finished = false;
+        lexicalComponents.push({
+          value: result[0],
+          type: lexicalComponent.type
+        });
+        content = removeFromString(result[0], content);
+        break;
+      }
+    }
+  }
+  while(!finished);
+  return lexicalComponents;
+};
+
+const removeFromString = (toRemove, from) => {
+  const size = toRemove.length;
+  return from.substring(size,from.size);
 };
 
 const getStyleTree = () => {
 
 };
 
-(() => {
+const main = (file) => {
   console.log('Inicia ejecución');
-  let htmlContent = readFile('./inputs/example1.html');
+  let htmlContent = readFile(file);
   console.log('Archivo sin quitar caracteres especiales');
   console.log('---------------------------------------------------------------------------------------------------------');
   console.log(htmlContent);
@@ -41,7 +107,13 @@ const getStyleTree = () => {
   console.log('---------------------------------------------------------------------------------------------------------');
   console.log(htmlContent);
   console.log('---------------------------------------------------------------------------------------------------------');
+  console.log('Generando componentes lexicos');
+  console.log('---------------------------------------------------------------------------------------------------------');
+  const lexicalComponents = getLexicalComponents(htmlContent);
+  console.log(lexicalComponents);
+  console.log('---------------------------------------------------------------------------------------------------------');
+  getHTMLTree(lexicalComponents);
 
-})();
+};
 
-module.exports = {readFile, getHTMLTree, getStyleTree};
+module.exports = {readFile, getHTMLTree, getStyleTree, main};
