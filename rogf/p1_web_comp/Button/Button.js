9c5346d3ac55291381ebdcf01f8a568520c90090
MEXEO\s2693301 <frg@scotiabank.com.mx>
2018-06-22 11:52:43 -0500

    IIFE add

--- rogf/p1_web_comp/Button/Button.js
+++ rogf/p1_web_comp/Button/Button.js
@@ -1,28 +1,32 @@
 'use strict';
-class ButtonToggle extends HTMLElement {
-    
-    constructor(){
-        super();
-        
-        const shadowRoot = this.attachShadow({mode: 'open'});
+(() => {
+    class ButtonToggle extends HTMLElement {
 
-        
-        const link = document.querySelector('#template_Button');
-        
-        // Clone the <template> in the import.
-        const template = link.import.querySelector('template');
-        const clone = document.importNode(template.content, true);
-        
-        shadowRoot.appendChild(clone);
-        
-        
-        this.addEventListener('click', () => {
-            this.dispatchEvent(new CustomEvent('toggle', {
-                bubbles: true,
-                composed: true
-            }));
-        });
-        
+        constructor() {
+            super();
+
+            const shadowRoot = this.attachShadow({
+                mode: 'open'
+            });
+
+
+            const link = document.querySelector('#template_Button');
+
+            // Clone the <template> in the import.
+            const template = link.import.querySelector('template');
+            const clone = document.importNode(template.content, true);
+
+            shadowRoot.appendChild(clone);
+
+
+            this.addEventListener('click', () => {
+                this.dispatchEvent(new CustomEvent('toggle', {
+                    bubbles: true,
+                    composed: true
+                }));
+            });
+
+        }
     }
-}
-window.customElements.define('button-toggle', ButtonToggle);
+    window.customElements.define('button-toggle', ButtonToggle);
+})();
--- rogf/p1_web_comp/List/List.js
+++ rogf/p1_web_comp/List/List.js
@@ -1,41 +1,45 @@
 'use strict';
-class LigthList extends HTMLElement {
-    
-    constructor(){
-        
-        super();
-                
-        const shadowRoot = this.attachShadow({mode: 'open'});
+(() => {
+    class LigthList extends HTMLElement {
 
-        
-        const link = document.querySelector('#template_List');
-        
-        // Clone the <template> in the import.
-        const template = link.import.querySelector('template');
-        const clone = document.importNode(template.content, true);
-        
-        
-        const list = clone.querySelector('ul');
-        
-        
-        let items = this.getAttribute('items');
-        items = JSON.parse(items);
-        
-        for(const item of items){
-            let li = document.createElement('li');
-            li.textContent = item;
-            list.appendChild(li);
+        constructor() {
+
+            super();
+
+            const shadowRoot = this.attachShadow({
+                mode: 'open'
+            });
+
+
+            const link = document.querySelector('#template_List');
+
+            // Clone the <template> in the import.
+            const template = link.import.querySelector('template');
+            const clone = document.importNode(template.content, true);
+
+
+            const list = clone.querySelector('ul');
+
+
+            let items = this.getAttribute('items');
+            items = JSON.parse(items);
+
+            for (const item of items) {
+                let li = document.createElement('li');
+                li.textContent = item;
+                list.appendChild(li);
+            }
+
+            shadowRoot.appendChild(clone.querySelector('style'));
+            shadowRoot.appendChild(document.createElement('button-toggle'));
+            shadowRoot.appendChild(list);
+
+
+            this.addEventListener('toggle', () => {
+
+                list.classList.toggle('hideList');
+            });
         }
-        
-        shadowRoot.appendChild(clone.querySelector('style'));  
-        shadowRoot.appendChild(document.createElement('button-toggle'));
-        shadowRoot.appendChild(list);
-        
-        
-        this.addEventListener('toggle', () => {
-
-            list.classList.toggle('hideList');
-        });
     }
-}
-window.customElements.define('light-list', LigthList);
+    window.customElements.define('light-list', LigthList);
+})();
